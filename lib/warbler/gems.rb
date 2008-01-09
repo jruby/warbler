868c34d1e326bada760cf23797f6aa027dcbaea8
Nick <nick@nicksieger.com>
2008-01-09 17:45:42 +0000

    Add new files from Robert
    git-svn-id: http://svn.caldersphere.net/svn/main/rubyforge/warbler/trunk@190 b03c2d0b-2f10-0410-a2f9-fc8001506dfa

---
+++ lib/warbler/gems.rb
@@ -0,0 +1,27 @@
+#--
+# (c) Copyright 2008 Robert Egglestone <r.egglestone@auckland.ac.nz>
+# See the file LICENSES.txt included with the distribution for
+# software license details.
+#++
+
+module Warbler
+  # A set of gems. This only exists to allow expected operations
+  # to be used to add gems, and for backwards compatibility.
+  # It would be easier to just use a hash.
+  class Gems < Hash
+    ANY_VERSION = nil
+    
+    def initialize(gems = nil)
+      if gems.is_a?(Hash)
+        self.merge!(gems)
+      elsif gems.is_a?(Array)
+        gems.each {|gem| self << gem }
+      end
+    end
+    
+    def <<(gem)
+      self[gem] ||= ANY_VERSION
+    end
+    
+  end
+end
---
+++ spec/warbler/gems_spec.rb
@@ -0,0 +1,32 @@
+#--
+# (c) Copyright 2008 Robert Egglestone <r.egglestone@auckland.ac.nz>
+# See the file LICENSES.txt included with the distribution for
+# software license details.
+#++
+
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Warbler::Gems do
+  it "should accept a hash for initialization" do
+    gems = Warbler::Gems.new({"actionpack" => "1.2.3"})
+    gems.should include("actionpack")
+    gems["actionpack"].should == "1.2.3"
+  end
+  
+  it "should accept an array for initialization" do
+    gems = Warbler::Gems.new ["activerecord"]
+    gems.should include("activerecord")
+  end
+  
+  it "should allow gems with a version" do
+    gems = Warbler::Gems.new
+    gems["actionpack"] = "> 1.2.3"
+    gems["actionpack"].should == "> 1.2.3"
+  end
+  
+  it "should allow gems without an explicit version" do
+    gems = Warbler::Gems.new
+    gems << "actionpack"
+    gems.should include("actionpack")
+  end  
+end
