20267618915f436119c4eee7df91c4543d1df26b
Nick Sieger <nick@nicksieger.com>
2008-11-07 08:28:48 -0500

    Mitigate rubygems compatibility issue (hat tip Jens Norrgrann)

--- lib/warbler/config.rb
+++ lib/warbler/config.rb
@@ -156,7 +156,9 @@ module Warbler
         @gems["rails"] = Rails::VERSION::STRING
       end
       if defined?(Rails.configuration.gems)
-        Rails.configuration.gems.each {|g| @gems[g.name] = g.version }
+        Rails.configuration.gems.each do |g|
+          @gems[g.name] = g.respond_to?(:version) ? g.version : g.specification.version
+        end
       end
       @webxml.jruby.max.runtimes = 1 if defined?(Rails.configuration.threadsafe!)
       true
