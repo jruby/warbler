fc8ac1c54371ccf4cca28866964f83a6ea1ec1f5
Nick Sieger <nick@nicksieger.com>
2009-08-26 19:43:08 -0500

    Upgrade jruby-rack jar, and get ready to cut 0.9.14

--- lib/warbler/version.rb
+++ lib/warbler/version.rb
@@ -1,9 +1,9 @@
 #--
-# (c) Copyright 2007-2008 Sun Microsystems, Inc.
+# (c) Copyright 2007-2009 Sun Microsystems, Inc.
 # See the file LICENSES.txt included with the distribution for
 # software license details.
 #++
 
 module Warbler
-  VERSION = "0.9.13"
+  VERSION = "0.9.14"
 end
