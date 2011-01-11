1d18c89ce574c6639d19539a1e5d9d5b978b84ea
Nick Sieger <nick@nicksieger.com>
2011-01-11 15:36:45 -0600

    Update copyright headers

--- lib/warbler/war.rb
+++ lib/warbler/war.rb
@@ -1,1 +1,8 @@
+#--
+# Copyright (c) 2010-2011 Engine Yard, Inc.
+# Copyright (c) 2007-2009 Sun Microsystems, Inc.
+# This source code is available under the MIT license.
+# See the file LICENSE.txt for details.
+#++
+
 require 'warbler/jar'
