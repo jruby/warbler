820e6bbf6eb3ecdbb280fbeacdf3d39fd85b5f1b
Nick Sieger <nick@nicksieger.com>
2012-03-09 21:23:33 -0600

    Update copyrights for 2012

--- lib/warbler/pathmap_helper.rb
+++ lib/warbler/pathmap_helper.rb
@@ -1,3 +1,10 @@
+#--
+# Copyright (c) 2010-2012 Engine Yard, Inc.
+# Copyright (c) 2007-2009 Sun Microsystems, Inc.
+# This source code is available under the MIT license.
+# See the file LICENSE.txt for details.
+#++
+
 module Warbler
   module PathmapHelper
     def apply_pathmaps(config, file, pathmaps)
