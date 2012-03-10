e929a35afd3e44dc2d04218082a5c3c4139c002a
Nick Sieger <nick@nicksieger.com>
2012-03-10 09:04:08 -0600

    More copyrights

--- ext/JarMain.java
+++ ext/JarMain.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2011 Engine Yard, Inc.
+ * Copyright (c) 2010-2012 Engine Yard, Inc.
  * Copyright (c) 2007-2009 Sun Microsystems, Inc.
  * This source code is available under the MIT license.
  * See the file LICENSE.txt for details.
