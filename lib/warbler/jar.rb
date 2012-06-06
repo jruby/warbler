3afa430dcff685d43fa115454fc685783d329e6d
Hawk Newton <hnewton@constantcontact.com>
2012-06-05 22:34:43 -0400

    Use the 1.9 flag for compilation if we're in 1.9 mode

--- lib/warbler/jar.rb
+++ lib/warbler/jar.rb
@@ -45,7 +45,8 @@ module Warbler
 
     def run_javac(config, compiled_ruby_files)
       # Need to use the version of JRuby in the application to compile it
-      %x{java -classpath #{config.java_libs.join(File::PATH_SEPARATOR)} org.jruby.Main -S jrubyc \"#{compiled_ruby_files.join('" "')}\"}
+      version_flag = RUBY_VERSION =~ /^1\.9/ ? '--1.9' : ''
+      %x{java -classpath #{config.java_libs.join(File::PATH_SEPARATOR)} org.jruby.Main #{version_flag} -S jrubyc \"#{compiled_ruby_files.join('" "')}\"}
     end
 
     def replace_compiled_ruby_files(config, compiled_ruby_files)
