5976a35c58a2680a73256a4aa4b468f62038fa7f
Peter Zotov <whitequark@whitequark.org>
2013-01-11 05:53:13 +0400

    Use specification.files to determine file list from gemspec.

--- lib/warbler/traits/gemspec.rb
+++ lib/warbler/traits/gemspec.rb
@@ -33,7 +33,7 @@ module Warbler
       end
 
       def update_archive(jar)
-        (Dir['**/*'] - config.compiled_ruby_files).each do |f|
+        (@spec.files - config.compiled_ruby_files).each do |f|
           jar.files[apply_pathmaps(config, f, :application)] = f
         end
         config.compiled_ruby_files.each do |f|
