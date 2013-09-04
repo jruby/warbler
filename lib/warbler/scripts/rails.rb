c57cd4a415f3071e5d883db0a32eaebb02caf98f
Joe Kutner <jpkutner@gmail.com>
2013-09-04 08:19:42 -0500

    Updated rails script to support Ruby 1.8. Fixes #168

--- lib/warbler/scripts/rails.rb
+++ lib/warbler/scripts/rails.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/env ruby
-APP_PATH = File.expand_path('../../config/application',  __FILE__)
-require_relative '../config/boot'
-require 'rails/commands'
+
+if RUBY_VERSION =~ /^1.8/
+  APP_PATH = File.expand_path('../../config/application',  __FILE__)
+  require File.expand_path('../../config/boot',  __FILE__)
+  require 'rails/commands' 
+else
+  APP_PATH = File.expand_path('../../config/application',  __FILE__)
+  require_relative '../config/boot'
+  require 'rails/commands'
+end
