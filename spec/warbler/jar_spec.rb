1e6c2ab03116af5f5e0b4da187732a3f85500414
Joe Kutner <jpkutner@gmail.com>
2014-02-13 20:33:54 -0600

    bumping some versions up. trying to make travis happy

--- spec/warbler/jar_spec.rb
+++ spec/warbler/jar_spec.rb
@@ -807,7 +807,7 @@ describe Warbler::Jar do
 
     context "in a Rack application" do
       before :each do
-        mkdir 'tmp' unless Dir.exists?('tmp')
+        mkdir 'tmp' unless File.directory?('tmp')
         Dir.chdir('tmp')
         rackup = "run Proc.new {|env| [200, {}, ['Hello World']]}"
         File.open("config.ru", "w") {|f| f << rackup }
