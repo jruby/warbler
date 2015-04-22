f019d98f867973f40ea77dae36256ac99adfb358
kares <self@kares.org>
2015-04-22 12:04:12 +0200

    polish *scripts/rails* - require_relative should no longer be used (on Ruby 2.x)

--- lib/warbler/scripts/rails.rb
+++ lib/warbler/scripts/rails.rb
@@ -1,11 +1,5 @@
 #!/usr/bin/env ruby
 
-if RUBY_VERSION =~ /^1.8/
-  APP_PATH = File.expand_path('../../config/application',  __FILE__)
-  require File.expand_path('../../config/boot',  __FILE__)
-  require 'rails/commands' 
-else
-  APP_PATH = File.expand_path('../../config/application',  __FILE__)
-  require_relative '../config/boot'
-  require 'rails/commands'
-end
+APP_PATH = File.expand_path('../../config/application',  __FILE__)
+require File.expand_path('../../config/boot',  __FILE__)
+require 'rails/commands'
