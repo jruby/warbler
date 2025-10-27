58849f248e572b7a2f7e0ab499f7ce959e1cdec1
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-10-27 17:39:33 +0800

    chore: remove compatibility code for ancient RubyGems and Bundler versions

--- lib/warbler/bundler_helper.rb
+++ lib/warbler/bundler_helper.rb
@@ -7,14 +7,7 @@
 module Warbler
   module BundlerHelper
     def to_spec(spec)
-      # JRuby <= 1.7.20 does not handle respond_to? with method_missing right
-      # thus a `spec.respond_to?(:to_spec) ? spec.to_spec : spec` won't do :
-      if ::Bundler.const_defined?(:StubSpecification) # since Bundler 1.10.1
-        spec = spec.to_spec if spec.is_a?(::Bundler::StubSpecification)
-      else
-        spec = spec.to_spec if spec.respond_to?(:to_spec)
-      end
-      spec
+      spec.respond_to?(:to_spec) ? spec.to_spec : spec
     end
     module_function :to_spec
   end
