d86b302a03acb4607be4db8eea892e638dc898b2
Marc Heiligers <marc@eternal.co.za>
2026-03-30 19:15:05 -0700

    Apply inline doc suggestion from @chadlwilson
    Co-authored-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- lib/warbler/config.rb
+++ lib/warbler/config.rb
@@ -29,8 +29,8 @@ module Warbler
     attr_accessor :features
 
     # Traits: an array of trait classes corresponding to
-    # characteristics of the project that are either auto-detected or
-    # configured.
+# characteristics of the project that are either auto-detected or
+# forced enabled during `Config.new(forced_traits: [...]) do |config|`
     attr_reader :traits
 
     # Directory where the war file will be written. Can be used to direct
