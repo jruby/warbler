0f7069ab78d118d6ceddfd7b71e3b3cc4c1c9ef0
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-09-01 14:50:30 +0800

    test: run tests across jruby-rack versions

--- integration/simple_rack_test/src/main/ruby/config/warble.rb
+++ integration/simple_rack_test/src/main/ruby/config/warble.rb
@@ -2,4 +2,5 @@ Warbler::Config.new do |config|
   config.autodeploy_dir = "../../../target"
   config.jar_name = "simple_rack_test-1.0"
   config.bundler = false
+  config.gems << Gem::Dependency.new("rack", "~> 2.2.0")
 end
