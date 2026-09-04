1027dd3ed4c3461bedf6b3da7f146698d6557968
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-09-04 16:01:59 +0800

    fix: disable rubyzip 3.x zip64 support for CRuby
    This breaks the produced jars/wars, by including fields that are not understood by Java. Currently this is not exposed
    since warbler basically only works with JRuby, and we have a native Java extension being used instead of rubyzip, but will
    fail as soon as the extension doesn't load, or trying to use CRuby.
    
    Setting this in case we remove the java extension as discussed at https://github.com/jruby/warbler/issues/580 as this bug would
    be exposed again. Could also use `suppress_extra_fields: false` however that will fail on RubyZip 3.0 -> 3.1.1 and doesn't seem worth it to construct a
    perfect dependency version range, or to test for the parameter.

--- lib/warbler/zip_support.rb
+++ lib/warbler/zip_support.rb
@@ -1,5 +1,8 @@
 require 'zip'
 
+# rubyzip >= 3 enables zip64 by default, adding zip64 extra fields that the `java -jar` launcher rejects
+Zip.write_zip64_support = false
+
 module Warbler
   class ZipSupport
     def self.create(filename, &blk)
